1bae46d48d569a6c70b6dd644c6d742a4d2be147
oshino <sinoazu1209@gmail.com>
2021-05-27 00:03:55 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+  const $drawer = $('.js-drawer');
+  const $anchorLink = $('.js-link');
+
+  $drawer.on('click', function() {
+    $('body').toggleClass('is-active');
+  });
+
+  $anchorLink.on('click', function() {
+    $('body').removeClass('is-active');
+  })
+});
